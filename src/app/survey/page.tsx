62f4d3107b9390083fe387260999de26bfc9608e
Alessandro Zanzi <alessandro.zanzi@usi.ch>
2023-12-10 15:27:45 +0100

    added countdown

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -6,6 +6,7 @@ import { type Task } from "@/lib/tasks";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { useTimer } from "react-timer-hook";
 
 export default function SurveyPage() {
   const searchParams = useSearchParams();
@@ -14,7 +15,16 @@ export default function SurveyPage() {
   const [task, setTask] = useState<Task>({ prompt: "", options: [] });
   const [time, setTime] = useState<number>(0);
   const [start, setStart] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+
+  const startTS = new Date();
+  startTS.setSeconds(startTS.getSeconds() + 3);
+
+  const countdown = useTimer({
+    expiryTimestamp: startTS,
+    onExpire: () => startTask(),
+    autoStart: false,
+  });
 
   const flow = searchParams.get("flow");
   if (!flow) {
@@ -23,40 +33,45 @@ export default function SurveyPage() {
 
   const getTask = async () => {
     try {
-      setIsLoading(true);
       const { data, status } = await axios.get(`/api/survey?flow=${flow}`);
       if (status === 204) {
         return router.push("/finished");
       }
       setTask(data.task);
-      setStart(true);
+      let nextTS = new Date();
+      nextTS.setSeconds(nextTS.getSeconds() + 3);
+      countdown.restart(nextTS, true);
     } catch (error) {
       console.log(error);
-    } finally {
-      setIsLoading(false);
     }
   };
 
   const recordTask = async (id: string) => {
     setStart(false);
-    setIsLoading(true);
+    setShowOptions(false);
     try {
       await axios.post(`/api/survey?flow=${flow}`, {
         answer: id,
         time,
       });
+      setTime(0);
       await getTask();
     } catch (error) {
       console.log(error);
-    } finally {
-      setIsLoading(false);
     }
   };
 
+  const startTask = () => {
+    setStart(true);
+    setShowOptions(true);
+  };
+
+  // initial fetch
   useEffect(() => {
     getTask();
   }, []);
 
+  // timer logic
   useEffect(() => {
     let interval: ReturnType<typeof setTimeout> | undefined = undefined;
     if (start) {
@@ -74,16 +89,13 @@ export default function SurveyPage() {
     <div className="h-full flex flex-col">
       <div className="flex flex-col items-center justify-center h-52 border-b border-slate-300 bg-slate-200">
         <p className="font-light text-lg">Click on the option you see</p>
-        {isLoading ? (
-          <Skeleton className="w-[300px] lg:w-[450px] h-12 bg-white rounded-full" />
-        ) : (
-          <h2 className="text-5xl font-bold">{task.prompt}</h2>
-        )}
+        <h2 className="text-5xl font-bold">{task?.prompt || ""}</h2>
       </div>
       <OptionKeyboard
         options={task.options}
         onOptClick={(id) => recordTask(id)}
-        isLoading={isLoading}
+        isLoading={!showOptions}
+        countdown={countdown.seconds}
       />
     </div>
   );
--- src/components/OptionKeyboard.tsx
+++ src/components/OptionKeyboard.tsx
@@ -6,16 +6,18 @@ import OptionCard from "./OptionCard";
 interface OptionKeyboardPrompts {
   options: string[];
   isLoading: boolean;
+  countdown: number;
   onOptClick: (id: string) => any;
 }
 
 export default function OptionKeyboard({
   options,
   isLoading = false,
+  countdown,
   onOptClick,
 }: OptionKeyboardPrompts) {
   return (
-    <div className="grow grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
+    <div className="relative grow grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
       {options.map((opt, i) => (
         <OptionCard
           onClick={() => onOptClick(opt)}
@@ -24,6 +26,13 @@ export default function OptionKeyboard({
           isLoading={isLoading}
         />
       ))}
+      {isLoading && (
+        <div className="absolute h-full w-full flex items-center justify-center bg-slate-100/60">
+          <div className="flex items-center justify-center h-[200px] w-[200px] rounded-full bg-slate-300 shadow-xl">
+            <span className="text-5xl font-light">{countdown}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
